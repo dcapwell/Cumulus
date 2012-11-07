30635fc974a721e24ef6784f6e0e22dc6a538f31
dcapwell <d.capwell@sisa.samsung.com>
2012-11-06 18:36:29 -0800

    Added a new RetryingFuture that will retry async tasks

---
+++ src/main/java/com/ekaqu/cumulus/retry/RetryingFuture.java
@@ -0,0 +1,135 @@
+package com.ekaqu.cumulus.retry;
+
+import com.google.common.base.Preconditions;
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
+import com.google.common.util.concurrent.AbstractFuture;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * A {@link java.util.concurrent.Future future} that will retry a task several times until a condition has been meet.
+ * For the user, they see a normal {@link ListenableFuture} but the value could be one of several attempts to execute
+ * the task where {@link #get()} refers to the final result of the successful task execution.
+ *
+ * @param <V> value type
+ */
+public final class RetryingFuture<V> extends AbstractFuture<V> {
+
+  /**
+   * The reference to the last thrown exception while trying to call the given task.
+   */
+  private final AtomicReference<Throwable> lastException = new AtomicReference<Throwable>();
+
+  /**
+   * The reference to the last known future returned from the task.
+   */
+  private final AtomicReference<ListenableFuture<V>> lastFuture = new AtomicReference<ListenableFuture<V>>();
+
+  /**
+   * Predicate to determine if the given value is usable or not.
+   */
+  private final Predicate<V> valuePredicate;
+
+  /**
+   * Creates a new Retrying Future.
+   *
+   * @param callable       task to execute
+   * @param maxRetries     to retry
+   * @param valuePredicate to filter out bad values
+   */
+  private RetryingFuture(final Callable<ListenableFuture<V>> callable, final int maxRetries,
+                         final Predicate<V> valuePredicate) {
+    Preconditions.checkArgument(maxRetries > 0, "Max Retries must be at least one.");
+    this.valuePredicate = Preconditions.checkNotNull(valuePredicate);
+    call(callable, maxRetries);
+  }
+
+  /**
+   * Creates a new Retrying Future.
+   *
+   * @param callable       task to execute
+   * @param maxRetries     to retry
+   * @param valuePredicate to filter out bad values
+   * @param <V>            value type
+   */
+  public static <V> RetryingFuture<V> create(final Callable<ListenableFuture<V>> callable, final int maxRetries,
+                                             final Predicate<V> valuePredicate) {
+    return new RetryingFuture<V>(callable, maxRetries, valuePredicate);
+  }
+
+  /**
+   * Creates a new Retrying Future.
+   *
+   * @param callable   task to execute
+   * @param maxRetries to retry
+   * @param <V>        value type
+   */
+  public static <V> RetryingFuture<V> create(final Callable<ListenableFuture<V>> callable, final int maxRetries) {
+    return new RetryingFuture<V>(callable, maxRetries, Predicates.<V>alwaysTrue());
+  }
+
+  /**
+   * Creates a new Retrying Future.
+   *
+   * @param callable task to execute
+   * @param <V>      value type
+   */
+  public static <V> RetryingFuture<V> create(final Callable<ListenableFuture<V>> callable) {
+    return new RetryingFuture<V>(callable, 3, Predicates.<V>alwaysTrue());
+  }
+
+  @Override
+  public boolean cancel(final boolean mayInterruptIfRunning) {
+    // stop the last future
+    final ListenableFuture<V> future = lastFuture.get();
+    if (future != null) {
+      future.cancel(true);
+    }
+    return super.cancel(mayInterruptIfRunning);
+  }
+
+  /**
+   * Calls the callable and adds a callback to retry or return a result.  This method uses recursion to implement
+   * retries.
+   *
+   * @param callable   task to run
+   * @param maxRetries to retry
+   */
+  private void call(final Callable<ListenableFuture<V>> callable, final int maxRetries) {
+    if (isDone() || maxRetries <= 0) {
+      // hit max retries, fail the future
+      if (lastException.get() != null) {
+        setException(lastException.get());
+      }
+      return;
+    }
+    try {
+      final ListenableFuture<V> future = callable.call();
+      lastFuture.set(future);
+      Futures.addCallback(future, new FutureCallback<V>() {
+        @Override
+        public void onSuccess(final V result) {
+          if (valuePredicate.apply(result)) {
+            set(result);
+          } else {
+            call(callable, maxRetries - 1);
+          }
+        }
+
+        @Override
+        public void onFailure(final Throwable t) {
+          lastException.set(t);
+          call(callable, maxRetries - 1);
+        }
+      });
+    } catch (Exception e) {
+      lastException.set(e);
+      call(callable, maxRetries - 1);
+    }
+  }
+}
---
+++ src/test/java/com/ekaqu/cumulus/retry/RetryingFutureTest.java
@@ -0,0 +1,106 @@
+package com.ekaqu.cumulus.retry;
+
+import com.google.common.base.Throwables;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.SettableFuture;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@Test(groups = "Unit")
+public class RetryingFutureTest {
+  private static final Logger LOGGER = LoggerFactory.getLogger(RetryingFutureTest.class.getName());
+
+  private final ExecutorService executorService = Executors.newSingleThreadExecutor(new ThreadFactoryBuilder().setDaemon(true).build());
+
+  @Test(timeOut = 3000)
+  public void simpleReturn() throws Exception {
+    final SettableFuture<String> future = SettableFuture.create();
+    Callable<ListenableFuture<String>> callable = mock(Callable.class);
+    when(callable.call()).thenReturn(future);
+
+    RetryingFuture<String> retryingFuture = RetryingFuture.create(callable);
+    executorService.submit(new Runnable() {
+      @Override
+      public void run() {
+        try {
+          TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+          throw Throwables.propagate(e);
+        }
+        future.set("value");
+      }
+    });
+    final String value = retryingFuture.get();
+
+    Assert.assertEquals(value, "value");
+  }
+
+  @Test(expectedExceptions = Exception.class, expectedExceptionsMessageRegExp = ".*Mock Exception")
+  public void failedCallable() throws Exception {
+    Callable<ListenableFuture<String>> callable = mock(Callable.class);
+    when(callable.call()).thenThrow(new Exception("Mock Exception"));
+
+    RetryingFuture<String> retryingFuture = RetryingFuture.create(callable);
+    retryingFuture.get();
+  }
+
+  @Test(expectedExceptions = IllegalArgumentException.class, expectedExceptionsMessageRegExp = ".*Max Retries must be at least one.*")
+  public void zeroRetry() throws Exception {
+    Callable<ListenableFuture<String>> callable = mock(Callable.class);
+    when(callable.call()).thenThrow(new Exception("Mock Exception"));
+
+    RetryingFuture<String> retryingFuture = RetryingFuture.create(callable, 0);
+    retryingFuture.get();
+  }
+
+  @Test(expectedExceptions = ExecutionException.class, expectedExceptionsMessageRegExp = ".*Task was cancelled\\.")
+  public void killUnderlineFuture() throws Exception {
+    final SettableFuture<String> future = SettableFuture.create();
+    Callable<ListenableFuture<String>> callable = mock(Callable.class);
+    when(callable.call()).thenReturn(future);
+
+    RetryingFuture<String> retryingFuture = RetryingFuture.create(callable);
+    executorService.submit(new Runnable() {
+      @Override
+      public void run() {
+        future.cancel(true);
+      }
+    });
+    retryingFuture.get();
+  }
+
+  public void killFuture() throws Exception {
+    final SettableFuture<String> future = SettableFuture.create();
+    Callable<ListenableFuture<String>> callable = mock(Callable.class);
+    when(callable.call()).thenReturn(future);
+
+    final RetryingFuture<String> retryingFuture = RetryingFuture.create(callable);
+    executorService.submit(new Runnable() {
+      @Override
+      public void run() {
+        retryingFuture.cancel(true);
+      }
+    });
+    try {
+      retryingFuture.get();
+      Assert.fail("Should have been canceled");
+    } catch (ExecutionException e) {
+      Assert.assertEquals(e.getCause().getClass(), CancellationException.class);
+    }
+
+    Assert.assertTrue(future.isCancelled());
+  }
+}
